4fdd77a9a80f8826f12ca87a6696930bdda12cd5
Iyaka <ini@mordu.ch>
2019-01-20 22:46:28 +0100

    Update

--- apprendreMath.py
+++ apprendreMath.py
@@ -2,15 +2,64 @@ import json
 import random
 # print("random :" + str(random.randint(0,20)))
 
-with open('parameters.json', 'r') as f:
-    parameters = json.load(f)
-# print(json.dumps(parameters, indent=4))
-# a= type(parameters)
+#Récupération des données pour les exercices
+with open('data.json', 'r') as file:
+    dataExercice = json.load(file)
+# print(json.dumps(dataExercice, indent=4))
+# a= type(dataExercice)
 # print(a)
-exercice = parameters["Multiplication"]
-print(exercice)
+
+# Récupération des exerices dans la liste titresExercices
+exercices = []
+for valeur in dataExercice.keys():
+    exercices.append(valeur)
+    # print("Exercices: " + str(titresExercices))
+
+# Affichage des exercices et selection
+nbrExercices = len(exercices)
+print(str(nbrExercices) + " exercices aux choix")
+index = 0
+for valeur in exercices:
+    print("[" + str(index) + "] Exercices: " + valeur)
+    index = index + 1
+
+reponseFausse = True
+while reponseFausse:
+    noExercice = int(input("Entrer le no de l'exercice: "))
+    if noExercice < nbrExercices:
+        reponseFausse = False
+print("Tu as choisis: " + exercices[noExercice])
+
+
+# Execution de l'exercice
+
+#Récupération des facteurs de multiplication de l exercice
+premierFacteurs = dataExercice[exercices[noExercice]]["premier facteurs"]
+deuxiemeFacteurs =  dataExercice[exercices[noExercice]]["deuxieme facteurs"]
+
+#Exercices
+indexCalcul = 1
+for facteur1 in premierFacteurs:
+    for facteur2 in deuxiemeFacteurs:
+        print("Combien font: " + str(facteur1)+"x"+str(facteur2)+"=")
+        
+
+        reponseFausse = True
+        nbrTentatives = 0
+        while reponseFausse :
+            reponse = int(input("réponse: "))
+            nbrTentatives = nbrTentatives + 1
+            # vérification de la réponse
+            if reponse == facteur1 * facteur2:
+                reponseFausse = False
+            else:
+                reponseFausse = True
+        print("Nombre de tentatives: " + str(nbrTentatives))
+##########################################################################
+exercice = dataExercice[exercices[0]]
+
 # premierL = exercice[0]
-# premierL = parameters["Multiplication"][0]
+# premierL = dataExercice["Multiplication"][0]
 # print(premierL)
 # premierFacteurList = premierL["list 1er facteur"]
 # print(premierFacteurList)
@@ -25,5 +74,3 @@ for facteur1 in premierFacteurList:
     for facteur2 in deuxiemeFacteurList:
         print("Multiplication " + str(facteur1) + "*" + str(facteur2) + " : " + str(facteur1 * facteur2))
     
-noExercice = input("Entrer le no de l'exercice: ")
-print(noExercice)
