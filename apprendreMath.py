fc2c0271504f841264deba25991a5aa34ed5077c
Iyaka <ini@mordu.ch>
2019-01-27 22:12:36 +0100

    update statistics

--- apprendreMath.py
+++ apprendreMath.py
@@ -1,20 +1,43 @@
 
 import json
 import random
-import time #for measuring elapsed time
+import time #for measuring elapsed time, date
 from func import *
 import os #for terminal screen clearing
 import winsound # Son, bruitage 
+import datetime #for date
 
-
+#Initialization
+currentDate = datetime.datetime.today()
+exerciceRecord = []
+calculsRecords = [] # enregistrement des calculs faux pour les statistiques
+print("Date: " + str(currentDate))
 # print("random :" + str(random.randint(0,20)))
+# Récupération des paramètres généraux
+with open("settings.json", "r") as file:
+    dataSettings = json.load(file)
+
+# Affichage et selection du joueur
+print("Joueurs: ")
+users = dataSettings["users"]
+for user in users:
+    print("[{index}] {user}".format(index = users.index(user), user = user )) 
+choixFaux = True
+while choixFaux:
+    joueurNo = captureNumber("Choix du joueur: ")
+    if joueurNo > len(users):
+        choixFaux = True
+    else:
+        choixFaux = False
+nomJoueur = users[joueurNo]
+print("Vous avez choisi: " + str(nomJoueur))
 
 # Récupération des données pour les exercices
-with open('data.json', 'r') as file:
+with open('exercices.json', 'r') as file:
     dataExercice = json.load(file)
-# print(json.dumps(dataExercice, indent=4))
-# a= type(dataExercice)
-# print(a)
+    # print(json.dumps(dataExercice, indent=4))
+    # a= type(dataExercice)
+    # print(a)
 
 # Récupération des exerices dans la liste titresExercices
 exercices = []
@@ -39,6 +62,7 @@ while reponseFausse:
     if noExercice < nbrExercices:
         reponseFausse = False
 print("Tu as choisis: " + exercices[noExercice])
+nomExerciceChoisi = exercices[noExercice]
 
 ##########################
 # Execution de l'exercice#
@@ -54,18 +78,21 @@ nombreDeCalculs = len(premierFacteurs) * len(deuxiemeFacteurs)
 os.system('cls' if os.name == 'nt' else 'clear')
 
 # Exercices
-indexCalcul = nombreDeCalculs
-tempsDepart = time.perf_counter()
+nombreCalculRestant = nombreDeCalculs
+tempsTotalDepart = time.perf_counter()
 nombreReponsesFaussesTot = 0
+indexCalcul = 0
 for facteur1 in premierFacteurs:
     for facteur2 in deuxiemeFacteurs:
- #       print("Entrer le résultat: " + str(facteur1)+"x"+str(facteur2)+"=")
-  #      print("Entrer le résultat:
-  #  {facteur1}x{facteur2}".format(facteur1=facteur1,facteur2=facteur2))        
+        #print("Entrer le résultat: " + str(facteur1)+"x"+str(facteur2)+"=")
+        #print("Entrer le résultat:
+        #{facteur1}x{facteur2}".format(facteur1=facteur1,facteur2=facteur2))        
         reponseFausse = True
         nbrTentatives = 0
+        tempsDepartCalcul = time.perf_counter()
+        recordCalcul = {}
         while reponseFausse :
-            reponse = captureNumber("[{countDown} calculs restant] Entrer le résultat de {facteur1}x{facteur2}: ".format(countDown=indexCalcul,facteur1=facteur1,facteur2=facteur2))
+            reponse = captureNumber("[{countDown} calculs restant] Entrer le résultat de {facteur1}x{facteur2}: ".format(countDown=nombreCalculRestant,facteur1=facteur1,facteur2=facteur2))
             nbrTentatives = nbrTentatives + 1
             # vérification de la réponse
             if reponse == facteur1 * facteur2:
@@ -75,13 +102,28 @@ for facteur1 in premierFacteurs:
                 reponseFausse = True
                 winsound.PlaySound('bad.wav', winsound.SND_FILENAME)
                 # print("Peux faire mieux ...")
-        indexCalcul = indexCalcul - 1
+        indexCalcul = indexCalcul + 1
+        nombreCalculRestant = nombreCalculRestant - 1
+        calcul = "{facteur1}x{facteur2}".format(facteur1=facteur1,facteur2=facteur2)
+        tempsFinCalcul = time.perf_counter()
+        dureeCalcul = tempsFinCalcul - tempsDepartCalcul
         print("Nombre de tentatives: " + str(nbrTentatives))
+
+        # Statistic calcul
+        recordCalcul = {"Calcul": calcul, "Nombre de tentatives": str(nbrTentatives), "Durée": str(dureeCalcul) }
+        if nbrTentatives > 1:
+            calculsRecords.append(recordCalcul) # ajout à la liste
         nombreReponsesFaussesTot = nombreReponsesFaussesTot + (nbrTentatives-1)
         #print("Nombre tentative: {nbrTent} et nombre total: {nbrTot}".format(nbrTent = nbrTentatives-1, nbrTot = nombreReponsesFaussesTot))
+    
+
+#Statistiques globales
+tempsTotalFin = time.perf_counter()
+dureeExercice = tempsTotalFin - tempsTotalDepart
 #Enregistrement des statistiques
-tempsFin = time.perf_counter()
-dureeExercice = tempsFin - tempsDepart
+#record = {"Date": , "Joueur": , "Nom exercice": , "Durée": , "Calcul": , "Nombre de tentative": }
+ 
+exerciceRecord = [str(currentDate), str(nomJoueur), str(nomExerciceChoisi), "Durée: " + str(dureeExercice), "Nombre réponses fausses totales: " + str(nombreReponsesFaussesTot), calculsRecords]
 
 print("Ouf.... c'est fini, temps passé: {tempsExercice}, Nombre de réponses fausses: {totalReponseFaux}".format(tempsExercice = dureeExercice,totalReponseFaux = nombreReponsesFaussesTot))
 
--- toDelete.py
+++ toDelete.py
@@ -6,3 +6,5 @@ print("hello" + str(start))
 end = time.perf_counter()
 elapsed = end - start
 print(elapsed)
+
+exerciceRecord = [str(currentDate), str(nomJoueur), str(nomExerciceChoisi), "Durée: " + str(dureeExercice), "Nombre réponses fausses totales: " + str(nombreReponsesFaussesTot)]
